de567b17052630b26c629adecb63219288673f0a
severinosiva.bc <severinosiva.bc@gmail.com>
2023-05-26 01:17:53 -0300

    Script de provisionamento de grupos e usuarios

---
+++ iacl.sh
@@ -0,0 +1,42 @@
+!#/bin/bash
+
+echo "Criando diretorios..."
+mkdir publico
+mkdir adm
+mkdir ven
+mkdir sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_VEN
+useradd sebastiao -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_VEN
+useradd robeto -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_VEN
+
+
+useradd josefina -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissoes de diretorios aos usuários..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+echo "Atribuindo permissoes aos diretorios..."
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Processo finalizado com sucesso!"
